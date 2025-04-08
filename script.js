5b1e47d4de5757f3d7eb71a64210fdb948368c90
linguagemsonhos <alinguagemdossonhos3@gmail.com>
2025-04-08 18:24:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
         
         if (email || phone) {
             if (phone && !phoneRegex.test(phone)) {
-                alert("Por favor, insira um WhatsApp válido: (DD)99999-9999 ou (DD) 99999-9999");
+                alert("Por favor, insira um WhatsApp válido: (DD)999999999 ou (DD) 99999-9999");
                 return;
             }
             
